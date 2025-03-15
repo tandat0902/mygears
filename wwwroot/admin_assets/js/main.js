ff7ab7af0b4861920b179a8e623842c8a2f5337b
tandat0902 <tandat.pham292@gmail.com>
2025-03-15 23:03:07 +0700

    [250315] Create HeaderPartialView

--- wwwroot/admin_assets/js/main.js
+++ wwwroot/admin_assets/js/main.js
@@ -1,14 +1,25 @@
 ﻿document.addEventListener("DOMContentLoaded", function () {
     document.querySelectorAll(".menu-dropdown").forEach(menu => {
         menu.addEventListener("click", function () {
-            let childMenu = this.querySelector(".child-menu");
+            
             document.querySelectorAll(".child-menu").forEach(child => {
                 if (child !== this.querySelector(".child-menu")) {
                     child.classList.remove("show");
                 }
             });
+            document.querySelectorAll(".icon-dropdown").forEach(icon => {
+                if (icon !== this.querySelector(".icon-dropdown")) {
+                    icon.classList.remove("rotate");
+                }
+            });
+
+            let childMenu = this.querySelector(".child-menu");
+            let icon = this.querySelector(".icon-dropdown");
             if (childMenu) {
                 childMenu.classList.toggle("show");
+            }
+            if (icon) {
+                icon.classList.toggle("rotate");
             }
         });
     });
