c471190a1b9aa0d2aa92052fe2245a43698de3a9
tandat0902 <tandat.pham292@gmail.com>
2025-03-15 14:21:17 +0700

    [250315] Update NavbarPartialView

---
+++ wwwroot/admin_assets/js/main.js
@@ -0,0 +1,21 @@
+﻿document.addEventListener("DOMContentLoaded", function () {
+    document.querySelectorAll(".menu-dropdown").forEach(menu => {
+        menu.addEventListener("click", function () {
+            let childMenu = this.querySelector(".child-menu");
+            document.querySelectorAll(".child-menu").forEach(child => {
+                if (child !== this.querySelector(".child-menu")) {
+                    child.classList.remove("show");
+                }
+            });
+            if (childMenu) {
+                childMenu.classList.toggle("show");
+            }
+        });
+    });
+});
+
+document.querySelectorAll(".child-menu").forEach(child => {
+    child.addEventListener("click", function (event) {
+        event.stopPropagation(); 
+    });
+});
